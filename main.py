3045ab71b4535232ca84602e7be7fbe8a8b077a5
kucicm <10778553+KucicM@users.noreply.github.com>
2023-11-20 18:18:24 +0100

    refactor computing of mapping

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-from PIL import Image, ImageFont, ImageDraw
+from collections import namedtuple
 import string
 
-def calculate_avg_pixel(char: str, font_size: int, font_name: str = "arial.ttf") -> float:
+
+from PIL import Image, ImageFont, ImageDraw
+
+PixelMapping = namedtuple("PixleMapping", ["chars", "weights"])
+
+def computeMapping() -> PixelMapping:
+    m = []
+    for char in string.printable:
+        if char in list("\t\n\r\x0b\x0c"): continue
+        m.append((mean_char_pixel(char), char))
+    m.sort()
+    return PixelMapping(chars=[c for _, c in m], weights=[(w - m[0][0]) / (m[-1][0] - m[0][0]) for w, _ in m])
+
+def mean_char_pixel(char: str, font_size: int = 20, font_name: str = "arial.ttf") -> float:
     font = ImageFont.truetype(font_name, font_size)
     _, _, x1, y1 = font.getbbox(char)
     img = Image.new('L', (x1, y1), color=255)
     draw = ImageDraw.Draw(img)
     draw.text((0, 0), char, font=font, fill=0)
     pixles = img.getdata()
-    return -(sum(pixles) // len(pixles)) / 255
+    return -sum(pixles) // len(pixles)
 
-chars = string.printable
-mapping = []
-for char in string.printable:
-    if char in list("\t\n\r\x0b\x0c"): continue
-    avg = calculate_avg_pixel(char, 20)
-    mapping.append((avg, char))
-mapping.sort()
-
-min_val, max_val = mapping[0][0], mapping[-1][0]
-mapping = [((v - min_val) / (max_val - min_val), c) for v, c in mapping]
-
+mapping = computeMapping()
 
 GRID_SIZE = (70, 70)
 
@@ -40,7 +43,7 @@ for _ in range(GRID_SIZE[1]):
     for _ in range(GRID_SIZE[0]):
         min_distance, min_char = float("inf"), ""
         pixel, i = img_pixels[i], i + 1
-        for val, char in mapping:
+        for val, char in zip(mapping.weights, mapping.chars):
             if (diff := abs(val - pixel)) < min_distance:
                 min_distance, min_char = diff, char
         print(min_char, end="")
